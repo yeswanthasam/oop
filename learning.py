0344184a6b4ff545ab188becb23fa2673383a588
yeswanth kumar <asamyeswanth1822@gmial.com>
2026-05-05 11:46:33 +0530

    l

--- learning.py
+++ learning.py
@@ -1,1 +1,13 @@
 print ("Hello World")
+
+'''list data type'''
+l=[1,2,3,4,5] #ordered
+print(l) 
+print(type(l)) 
+print(l[-1]) #accessing single element
+
+#list are mutable
+l.append(6) #insert element
+print(l)   
+l.remove(6) #remove element
+print(l)
